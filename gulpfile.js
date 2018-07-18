134bd25ce1bd095cc72810c23a5226ef5523c1e0
zfq <zfq0714@qq.com>
2018-07-18 17:03:13 +0800

    zfq

--- gulpfile.js
+++ gulpfile.js
@@ -1,88 +1,101 @@
-var gulp     = require('gulp'),  
-    concat   = require('gulp-concat'),//- 多个文件合并为一个；  
-    cleanCSS = require('gulp-clean-css'),//- 压缩CSS为一行；  
-    ugLify   = require('gulp-uglify'),//压缩js  
-    imageMin = require('gulp-imagemin'),//压缩图片  
+var gulp = require('gulp'),
+    concat = require('gulp-concat'), //- 多个文件合并为一个；  
+    cleanCSS = require('gulp-clean-css'), //- 压缩CSS为一行；  
+    uglify = require('gulp-uglify'), //压缩js  
+    imageMin = require('gulp-imagemin'), //压缩图片  
     pngquant = require('imagemin-pngquant'), // 深度压缩  
-    htmlMin  = require('gulp-htmlmin'),//压缩html  
-    changed  = require('gulp-changed'),//检查改变状态  
-    less     = require('gulp-less'),//压缩合并less  
-    del      = require('del'), 
+    htmlMin = require('gulp-htmlmin'), //压缩html  
+    changed = require('gulp-changed'), //检查改变状态  
+    less = require('gulp-less'), //压缩合并less  
+    del = require('del'),
     rename = require('gulp-rename');
-    browserSync = require("browser-sync").create(),//浏览器实时刷新 
+    browserSync = require("browser-sync").create(), //浏览器实时刷新 
     babel = require("gulp-babel"),
-    watch = require("gulp-watch"); 
-  
-//删除dist下的所有文件  
-gulp.task('delete',function(cb){  
-    return del(['dist/*','!dist/images'],cb);  
-})  
-  
-//压缩html  
-gulp.task('html', function () {  
-    var options = {  
-        removeComments: true,//清除HTML注释  
-        collapseWhitespace: true,//压缩HTML  
-        removeScriptTypeAttributes: true,//删除<script>的type="text/javascript"  
-        removeStyleLinkTypeAttributes: true,//删除<style>和<link>的type="text/css"  
-        minifyJS: true,//压缩页面JS  
-        minifyCSS: true//压缩页面CSS  
-    };  
-    gulp.src('src/*.html')  
-        .pipe(changed('dist', {hasChanged: changed.compareSha1Digest}))  
-        .pipe(htmlMin(options))  
-        .pipe(gulp.dest('dist'))  
-        .pipe(browserSync.reload({stream:true}));  
-});  
-  
-//实时编译less  
-gulp.task('less', function () {  
-    gulp.src(['./src/less/*.less']) //多个文件以数组形式传入  
-        .pipe(changed('dist/css', {hasChanged: changed.compareSha1Digest}))  
-        .pipe(less())//编译less文件  
-        .pipe(cleanCSS())//压缩新生成的css  
-        .pipe(gulp.dest('dist/css'))//将会在css下生成main.css  
-        .pipe(browserSync.reload({stream:true}));  
-});  
-  
-//压缩js  
-gulp.task("script",function(){  
-    gulp.src('./src/js/*.js')
-        .pipe(changed('dist/js', {hasChanged: changed.compareSha1Digest}))  
-        .pipe(babel())
-        .pipe(ugLify())  
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('dist/js'))  
-        .pipe(browserSync.reload({stream:true}));  
-});  
-  
-// 压缩图片  
-gulp.task('images', function () {  
-    gulp.src('./src/images/*.*')  
-        .pipe(changed('dist/images', {hasChanged: changed.compareSha1Digest}))  
-        .pipe(imageMin({  
-            progressive: true,// 无损压缩JPG图片  
-            svgoPlugins: [{removeViewBox: false}], // 不移除svg的viewbox属性  
-            use: [pngquant()] // 使用pngquant插件进行深度压缩  
-        }))  
-        .pipe(gulp.dest('dist/images'))  
-        .pipe(browserSync.reload({stream:true}));  
-});  
+    watch = require("gulp-watch"),
+    browserify = require('browserify'),
+    source = require('vinyl-source-stream'),  //steam流
+    buffer = require('vinyl-buffer'),  //buffer流
+    glob = require('glob');     //遍历文件
 
-  
+//删除dist下的所有文件  
+gulp.task('delete', function(cb) {
+    return del(['dist/*', '!dist/images'], cb);
+})
+
+//压缩html  
+gulp.task('html', function() {
+    var options = {
+        removeComments: true, //清除HTML注释  
+        collapseWhitespace: true, //压缩HTML  
+        removeScriptTypeAttributes: true, //删除<script>的type="text/javascript"  
+        removeStyleLinkTypeAttributes: true, //删除<style>和<link>的type="text/css"  
+        minifyJS: true, //压缩页面JS  
+        minifyCSS: true //压缩页面CSS  
+    };
+    gulp.src('src/*.html')
+        .pipe(changed('dist', { hasChanged: changed.compareSha1Digest }))
+        .pipe(htmlMin(options))
+        .pipe(gulp.dest('dist'))
+        .pipe(browserSync.reload({ stream: true }));
+});
+
+//实时编译less  
+gulp.task('less', function() {
+    gulp.src(['./src/less/*.less']) //多个文件以数组形式传入  
+        .pipe(changed('dist/css', { hasChanged: changed.compareSha1Digest }))
+        .pipe(less()) //编译less文件  
+        .pipe(cleanCSS()) //压缩新生成的css  
+        .pipe(gulp.dest('dist/css')) //将会在css下生成main.css  
+        .pipe(browserSync.reload({ stream: true }));
+});
+
+// 压缩图片  
+gulp.task('images', function() {
+    gulp.src('./src/images/*.*')
+        .pipe(changed('dist/images', { hasChanged: changed.compareSha1Digest }))
+        .pipe(imageMin({
+            progressive: true, // 无损压缩JPG图片  
+            svgoPlugins: [{ removeViewBox: false }], // 不移除svg的viewbox属性  
+            use: [pngquant()] // 使用pngquant插件进行深度压缩  
+        }))
+        .pipe(gulp.dest('dist/images'))
+        .pipe(browserSync.reload({ stream: true }));
+});
+
+//browserify加载包到本地，babel转换到es5
+gulp.task("script", function(cb) {
+    glob('./src/js/*.js', function(err, files) {
+        var b = browserify();
+        files.forEach(function(file) {
+          b.add(file);
+        });
+        b.transform("babelify", { presets: ["es2015"] })
+          .bundle()
+          .pipe(source('bundle.js'))
+          .pipe(buffer())
+          .pipe(uglify())
+          .pipe(gulp.dest('./dist/js'))
+          .pipe(browserSync.reload({ stream: true }));
+
+        cb();
+     })
+    
+});
+
+
 //启动热更新  
-gulp.task('serve', ['delete'], function() {  
-    gulp.start('script','less','html');  
-    browserSync.init({  
-        port: 2018,  
-        server: {  
-            baseDir: ['dist']  
-        }  
-    });  
-    gulp.watch('src/js/*.js', ['script']);         //监控文件变化，自动更新  
-    gulp.watch('src/less/*.less', ['less']);  
-    gulp.watch('src/*.html', ['html']);  
-    gulp.watch('src/images/*.*', ['images']);  
-});  
-  
-gulp.task('default',['serve']); 
+gulp.task('serve', ['delete'], function() {
+    gulp.start('script', 'less', 'html');
+    browserSync.init({
+        port: 2018,
+        server: {
+            baseDir: ['dist']
+        }
+    });
+    gulp.watch('src/js/*.js', ['script']); //监控文件变化，自动更新  
+    gulp.watch('src/less/*.less', ['less']);
+    gulp.watch('src/*.html', ['html']);
+    gulp.watch('src/images/*.*', ['images']);
+});
+
+gulp.task('default', ['serve']);
--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,174 @@
-const {Scene, Sprite} = spritejs
+(async function () {
+  const {Scene, Sprite, Group} = spritejs
+  const scene = new Scene('#paper', {viewport: ['auto', 'auto'], resolution: [1200, 1200]})
 
-const scene = new Scene('#demo-quickStart', {viewport: [770, 200], resolution: [3080, 800]})
+  await scene.preload([
+    'https://p5.ssl.qhimg.com/t01f47a319aebf27174.png',
+    'https://s3.ssl.qhres.com/static/a6a7509c33a290a6.json',
+  ])
 
-const layer = scene.layer()
+  const bglayer = scene.layer('bg', {
+      handleEvent: false,
+    }),
+    fglayer = scene.layer('fg', {
+      handleEvent: false,
+      renderMode: 'repaintDirty',
+    })
 
-const robot = new Sprite('https://p5.ssl.qhimg.com/t01c33383c0e168c3c4.png')
+  const center = [600, 640]
+  const region = new Group()
 
-robot.attr({
-  anchor: [0, 0.5],
-  pos: [0, 0],
-})
+  region.attr({
+    anchor: 0.5,
+    size: [920, 920],
+    pos: center,
+    bgcolor: '#f4f2e6',
+    borderRadius: 120,
+  })
+  bglayer.append(region)
 
-robot.animate([
-  {pos: [0, 0]},
-  {pos: [0, 300]},
-  {pos: [2700, 300]},
-  {pos: [2700, 0]},
-], {
-  duration: 5000,
-  iterations: Infinity,
-  direction: 'alternate',
-})
+  const robot = new Sprite('guanguan1.png')
+  robot.attr({
+    anchor: [0.5, 1],
+    pos: [280, 600],
+    transformOrigin: [0, -300],
+    scale: 0.4,
+  })
+  fglayer.append(robot)
 
-layer.append(robot)
+  async function robotMotion() {
+    await robot.animate([
+      {textures: 'guanguan1.png'},
+      {textures: 'guanguan2.png'},
+      {textures: 'guanguan3.png'},
+      {textures: 'guanguan1.png'},
+      {textures: 'guanguan2.png'},
+      {textures: 'guanguan3.png', scale: [-0.4, 0.4]},
+      {textures: 'guanguan1.png', scale: [0.4, 0.4]},
+    ], {
+      duration: 3000,
+      easing: 'step-end',
+      fill: 'forwards',
+    }).finished
+
+    await robot.animate([
+      {y: 600},
+      {y: 1280},
+    ], {
+      duration: 500,
+      easing: 'ease-in',
+      fill: 'forwards',
+    }).finished
+
+    await robot.animate([
+      {textures: 'guanguan1.png'},
+      {textures: 'guanguan3.png', scale: [-0.4, 0.4]},
+      {textures: 'guanguan1.png'},
+      {textures: 'guanguan2.png'},
+      {textures: 'guanguan3.png', scale: [0.4, 0.4]},
+    ], {
+      duration: 2000,
+      easing: 'step-end',
+      fill: 'forwards',
+    }).finished
+
+    await robot.animate([
+      {x: 280},
+      {x: 940},
+    ], {
+      duration: 2000,
+      easing: 'ease-in-out',
+      fill: 'forwards',
+    }).finished
+
+    await robot.animate([
+      {rotate: 0},
+      {rotate: -70},
+    ], {
+      duration: 1000,
+      easing: 'ease-in-out',
+      direction: 'alternate',
+      iterations: 2,
+      fill: 'forwards',
+    }).finished
+
+    await robot.animate([
+      {textures: 'guanguan3.png'},
+      {textures: 'guanguan1.png'},
+      {textures: 'guanguan2.png'},
+      {textures: 'guanguan1.png'},
+      {textures: 'guanguan3.png'},
+    ], {
+      duration: 2500,
+      easing: 'step-end',
+      fill: 'forwards',
+    }).finished
+
+    await robot.animate([
+      {rotate: 0},
+      {rotate: -70},
+    ], {
+      duration: 1000,
+      easing: 'ease-in-out',
+      direction: 'alternate',
+      iterations: 2,
+      fill: 'forwards',
+    }).finished
+
+    await robot.animate([
+      {textures: 'guanguan3.png'},
+      {textures: 'guanguan1.png'},
+      {textures: 'guanguan2.png'},
+      {textures: 'guanguan1.png'},
+      {textures: 'guanguan3.png', scale: [-0.4, 0.4]},
+    ], {
+      duration: 2500,
+      easing: 'step-end',
+      fill: 'forwards',
+    }).finished
+
+    await robot.animate([
+      {x: 940},
+      {x: 280, offset: 0.99, scale: [-0.4, 0.4]},
+      {x: 280, scale: [0.4, 0.4]},
+    ], {
+      duration: 1000,
+      easing: 'ease-in-out',
+      fill: 'forwards',
+    }).finished
+
+    await robot.animate([
+      {x: 280, y: 1280, rotate: 0},
+      {x: 940, y: 1280, rotate: 0, offset: 0.3},
+      {x: 940, y: 1280, rotate: -90, offset: 0.35},
+      {x: 940, y: 600, rotate: -90, offset: 0.65},
+      {x: 940, y: 600, rotate: -180, offset: 0.7},
+      {x: 280, y: 600, rotate: -180},
+    ], {
+      delay: 500,
+      duration: 3500,
+      easing: 'ease-in',
+      fill: 'forwards', 
+    }).finished
+
+    robot.attr({
+      textures: 'guanguan1.png',
+    })
+
+    await robot.animate([
+      {y: 600, rotate: -180},
+      {y: 600, rotate: 0},
+    ], {
+      delay: 1000,
+      duration: 1000,
+      fill: 'forwards',
+    }).finished
+  }
+
+  // noprotect
+  while(1) {
+    /* eslint-disable no-await-in-loop */
+    await robotMotion()
+    /* eslint-enable no-await-in-loop */
+  }
+}())
